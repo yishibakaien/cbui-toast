944127f07e980070854d46604d75fb91698fcd8b
cloud-cb <646932161@qq.com>
2017-07-11 10:52:15 +0800

    增加npm dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 'use strict';
+var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -7,7 +8,22 @@ module.exports = {
     output: {
         path: __dirname + '/build/',
         filename: 'blackTip.js',
+        chunkFilename: 'blackTip.js',
         library: 'blackTip',
         libraryTarget: 'umd'
-    }
+    },
+    devServer: {
+        port: 4000,
+        host: '127.0.0.1',
+        inline: true,
+        hot: false
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './index.html',
+            filename: 'index.html',
+            chunks: ['app'],
+            inject: true
+        })
+    ]
 }
